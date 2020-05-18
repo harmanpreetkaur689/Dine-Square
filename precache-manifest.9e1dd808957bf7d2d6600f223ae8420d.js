1adb8152bffcc66338c45dbeb583ae32d1261fa7
harmanpreetkaur689 <harmanpreetkaur689@gmail.com>
2020-05-19 00:52:44 +0530

    Updates

---
+++ precache-manifest.9e1dd808957bf7d2d6600f223ae8420d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4963fca443a4a7491231ca47a20bc1fa",
+    "url": "/canteen-ordering-project/index.html"
+  },
+  {
+    "revision": "e03b28964fb62ff6ed2d",
+    "url": "/canteen-ordering-project/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "bf8cd5b478c9379313b8",
+    "url": "/canteen-ordering-project/static/css/main.9e77079b.chunk.css"
+  },
+  {
+    "revision": "e03b28964fb62ff6ed2d",
+    "url": "/canteen-ordering-project/static/js/2.81c989ca.chunk.js"
+  },
+  {
+    "revision": "b9bf62f5644947be677f45bff7ad1106",
+    "url": "/canteen-ordering-project/static/js/2.81c989ca.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bf8cd5b478c9379313b8",
+    "url": "/canteen-ordering-project/static/js/main.aa8ec9a1.chunk.js"
+  },
+  {
+    "revision": "d8db87174904d90bcee6",
+    "url": "/canteen-ordering-project/static/js/runtime-main.35845c68.js"
+  },
+  {
+    "revision": "e991edccaf28cc12e89c0f768ab01af3",
+    "url": "/canteen-ordering-project/static/media/newbg.e991edcc.jpg"
+  }
+]);
